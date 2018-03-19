57cdd2b3cf474d8b068249d921deaa8a6f396056
Dzhabrail Elmezov <elmezov007@gmail.com>
2018-03-19 13:30:53 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -9,16 +9,13 @@ vector_t::vector_t() : elements_(nullptr), size_(0), capacity_(0)
 
 vector_t::vector_t(vector_t const & other)
 {
-
-	elements_ = new int [other.capacity_];
-	for (size_t i = 0; i < other.size_; i++)
-	{
-		elements_[i] = other.elements_[i];
-	}	
-	
-	capacity_ = other.capacity_;
-	size_ = other.size_;
-	
+    	elements_ = new int[other.capacity_];
+    	for (std::size_t i = 0; i < other.size_; i++)
+    	{
+        	elements_[i] = other.elements_[i];
+    	}
+    	capacity_ = other.capacity_;
+	size_ = other.size_
 }
 
 vector_t & vector_t::operator =(vector_t const & other)
@@ -110,7 +107,7 @@ void vector_t::pop_back()
 		capacity_ = capacity_ / 2;
 		size_--;
 		int * temp_elements = new int [capacity_];
-		for (int i = 0; i < size_; i++)
+		for (size_t i = 0; i < size_; i++)
 			temp_elements[i] = elements_[i];
 		
 		delete[]elements_;
